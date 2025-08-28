72c485da86c4900371c7db4007e3adc5340635b4
YagoSobreira <93669303+YagoSobreira@users.noreply.github.com>
2025-08-28 13:23:52 -0400

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("99999-1111", "Nokia Tijolão", "123456789", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("99999-2222", "iPhone 14", "987654321", 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
